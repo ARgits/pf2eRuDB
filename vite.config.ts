9dc1427de480005a018648ec0a4e57cd62314162
ARgits <kydr95.mail.ru@gmail.com>
2023-11-04 13:56:29 +0300

    change vite.config.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  assetsInclude: "**/*.html",
+  plugins: [svelte(), splitVendorChunkPlugin()],
+  build: {
+    outDir: "dist",
+    dynamicImportVarsOptions:{
+      include:'src/main.ts'
+    }
+  },
+  //assetsInclude: "dist/assets/*.html",
 });
