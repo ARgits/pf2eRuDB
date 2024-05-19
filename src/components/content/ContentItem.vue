ba94fc110e1fde72274ef8f4a317377624f1074b
ARgits <kydr95.mail.ru@gmail.com>
2024-05-19 21:51:55 +0300

    change modal item card behavior: description shown by default (instead of non-modal item card)

--- src/components/content/ContentItem.vue
+++ src/components/content/ContentItem.vue
@@ -1,116 +1,118 @@
 <script setup lang="ts">
-import { ref, type Ref, } from "vue"
+import {ref, type Ref,} from "vue"
 
-import type { generalContent } from '@types';
+import type {generalContent} from '@types';
 import ContentDescription from "./ContentDescription.vue";
 import ContentTraits from "./ContentTraits.vue";
 import ContainerSlideTransition from "@components/transitions/ContainerSlideTransition.vue";
 import FavoriteButton from "./FavoriteButton.vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faCaretDown, faSquareXmark} from "@fortawesome/free-solid-svg-icons";
 import RotateTransition from "@components/transitions/RotateTransition.vue";
-import { useModalWindows } from "@stores/modalWindows";
-import { faSquareXmark } from '@fortawesome/free-solid-svg-icons';
+import {useModalWindows} from "@stores/modalWindows";
+
 const props = defineProps<{ content: generalContent, modal?: boolean }>()
 
-const { closeModal } = useModalWindows()
+const {closeModal} = useModalWindows()
 
 const itemElem: Ref<HTMLDivElement | null> = ref(null);
 const nameElem: Ref<HTMLDivElement | null> = ref(null);
 // const { height, width } = useElementSize(itemElem)
-const isDescVisible = ref(false)
-function showHideDesc() {
-    isDescVisible.value = !isDescVisible.value
-    console.log(props.content)
-    if (isDescVisible.value) {
-        itemElem.value!.scrollIntoView({ behavior: 'smooth' })
+const isDescVisible = ref(props.modal ?? false)
 
-    } else {
-        itemElem.value!.children.item(1)!.scrollTop = 0
-    }
+function showHideDesc() {
+  isDescVisible.value = !isDescVisible.value
+  console.log(props.content)
+  if (isDescVisible.value) {
+    itemElem.value!.scrollIntoView({behavior: 'smooth'})
+
+  } else {
+    itemElem.value!.children.item(1)!.scrollTop = 0
+  }
 }
+
 function close() {
-    closeModal(props.content.id)
+  closeModal(props.content.id)
 }
 </script>
 <template>
-    <div class="content_item" ref="itemElem">
-        <div class="name" ref="nameElem">
-            <button @click="close" class="close" v-if="modal">
-                <FontAwesomeIcon :icon="faSquareXmark"></FontAwesomeIcon>
-            </button>
-            <FavoriteButton :id="content.id" :dataType="content.dataType"></FavoriteButton>
+  <div class="content_item" ref="itemElem">
+    <div class="name" ref="nameElem">
+      <button @click="close" class="close" v-if="modal">
+        <FontAwesomeIcon :icon="faSquareXmark"></FontAwesomeIcon>
+      </button>
+      <FavoriteButton :id="content.id" :dataType="content.dataType"></FavoriteButton>
 
-            <div v-if="content.name">
-                <div>{{ content.name }}</div>
-                <div>{{ content.originalName }}</div>
-                <div v-if="'level' in content">Уровень: {{ content.level }}</div>
-            </div>
-            <div v-else v-html="content.fullName"></div>
-            <hr />
-        </div>
-        <ContentTraits v-if="content.traits?.length" :content="content"></ContentTraits>
-        <button @click="showHideDesc">
-            <RotateTransition angle="180deg" :trigger="isDescVisible">
-                <FontAwesomeIcon :icon="faCaretDown"></FontAwesomeIcon>
-            </RotateTransition>
-            Описание
-            <RotateTransition angle="180deg" :trigger="isDescVisible">
-                <FontAwesomeIcon :icon="faCaretDown"></FontAwesomeIcon>
-            </RotateTransition>
-        </button>
-        <ContainerSlideTransition>
-            <ContentDescription v-if="isDescVisible" :description="content.desc">
-            </ContentDescription>
-        </ContainerSlideTransition>
-
-
+      <div v-if="content.name">
+        <div>{{ content.name }}</div>
+        <div>{{ content.originalName }}</div>
+        <div v-if="'level' in content">Уровень: {{ content.level }}</div>
+      </div>
+      <div v-else v-html="content.fullName"></div>
+      <hr/>
     </div>
+    <ContentTraits v-if="content.traits?.length" :content="content"></ContentTraits>
+    <button @click="showHideDesc">
+      <RotateTransition angle="180deg" :trigger="isDescVisible">
+        <FontAwesomeIcon :icon="faCaretDown"></FontAwesomeIcon>
+      </RotateTransition>
+      Описание
+      <RotateTransition angle="180deg" :trigger="isDescVisible">
+        <FontAwesomeIcon :icon="faCaretDown"></FontAwesomeIcon>
+      </RotateTransition>
+    </button>
+    <ContainerSlideTransition>
+      <ContentDescription v-if="isDescVisible" :description="content.desc">
+      </ContentDescription>
+    </ContainerSlideTransition>
+
+
+  </div>
 </template>
 <style lang="scss" scoped>
 .content_item {
-    border: 1px solid black;
-    border-radius: var(--border-radius);
+  border: 1px solid black;
+  border-radius: var(--border-radius);
 
-    padding: 5px;
-    width: calc(100% - 12px);
-    max-width: 100%;
-    max-height: 500px;
-    scrollbar-gutter: stable;
-    height: min-content;
-    display: flex;
-    flex-direction: column;
-    transition: 1s;
-    background: var(--background-img);
+  padding: 5px;
+  width: calc(100% - 12px);
+  max-width: 100%;
+  max-height: 500px;
+  scrollbar-gutter: stable;
+  height: min-content;
+  display: flex;
+  flex-direction: column;
+  transition: 1s;
+  background: var(--background-img);
 
-    &>button {
-        background-color: hsla(0, 0%, 0%, .1);
-    }
+  & > button {
+    background-color: hsla(0, 0%, 0%, .1);
+  }
 }
 
 b .close {
-    display: inline;
-    padding: 0;
-    float: right;
-    border: 0;
-    background: none;
-    margin: 5px;
-    height: 1.5rem;
+  display: inline;
+  padding: 0;
+  float: right;
+  border: 0;
+  background: none;
+  margin: 5px;
+  height: 1.5rem;
 }
 
 .name {
 
-    &>div {
-        font-size: 110%;
-        display: flex;
-        flex-wrap: wrap;
-        gap: 10px;
-    }
+  & > div {
+    font-size: 110%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
 }
 
-@media (max-aspect-ratio:1/1) {
-    .content_item {
-        --max-width: 100%;
-    }
+@media (max-aspect-ratio: 1/1) {
+  .content_item {
+    --max-width: 100%;
+  }
 }
 </style>
